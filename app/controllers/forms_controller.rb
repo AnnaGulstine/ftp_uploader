411240b0dc073fec20d1f1e26437e1ccd17d1b9a
AnnaGulstine <annagulstine@gmail.com>
2019-02-03 12:44:21 -0600

    Style form page, add success message, tweak functionality

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,22 +1,32 @@
 require 'net/ftp'
 
 class FormsController < ApplicationController
-  def index
-  end
-
   def new
   end
 
   def upload
     uploaded_content = params[:content]
-    File.open(Rails.root.join('public', 'uploads', uploaded_content.original_filename), 'wb') do |file|
-      file.write(uploaded_content.read)
-      ftp = Net::FTP.new('ftpupload.net')
-      ftp.login(user = ENV['FTP_USERNAME'], passwd = ENV['FTP_PASSWORD'])
-      ftp.passive = true
-      ftp.chdir('/htdocs')
-      ftp.putbinaryfile(file)
-      ftp.quit()
+    file_name = params[:file_name]
+    if file_name == ''
+      file_name = uploaded_content.original_filename
     end
+    File.open(Rails.root.join('public', 'uploads', file_name), 'wb') do |file|
+      ftpClient = Net::FTP.new('ftpupload.net')
+      ftpClient.login(user = ENV['FTP_USERNAME'], passwd = ENV['FTP_PASSWORD'])
+      ftpClient.passive = true
+      ftpClient.chdir('/htdocs/uploaded_through_ftp_uploader_app')
+      ftpClient.putbinaryfile(file)
+      format_message_and_redirect(ftpClient.last_response)
+      ftpClient.quit()
+    end
+  end
+
+  def format_message_and_redirect(ftp_response)
+    if ftp_response.include? '226'
+      flash[:success] = ftp_response
+    else
+      flash[:error] = ftp_response
+    end
+    redirect_to '/'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => 'forms#index'
+  get '/' => 'forms#new'
   get '/forms/new' => 'forms#new'
-  post '/forms' => 'forms#create'
   post '/upload' => 'forms#upload'
 end
